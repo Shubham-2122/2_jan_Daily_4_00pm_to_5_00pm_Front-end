0dd90dab016b462c6c6a03bb3a7f2f8300cd5580
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-03-10 16:41:09 +0530

    js

--- Javascript/ex.js
+++ Javascript/ex.js
@@ -1,1 +1,12 @@
-console.log("hello javascipt")
+console.log("hello javascipt");
+
+// console.log(document.getElementById("h1"))
+
+document.getElementById("h1").innerText = "hello external js";
+
+// bom bowser object model
+// alert("pls field form");
+// confirm("are your sure ?")
+// prompt("5 + 5 ?")
+
+// print()
