54e5866ea0f02531a49cf17bcc87249027f51613
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-05-14 16:45:04 +0530

    react

--- React/demo/src/App.js
+++ React/demo/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Classcompo from "./Compent/Classcompo";
+import Funcompo from "./Compent/Funcompo";
 
-function App(){
-    return(
-        <h1>hello this App data</h1>
+function App() {
+    return (
+        <div>
+            <h1>hello this App data</h1>
+            <h1>hello this h1</h1>
+            <Classcompo />
+            <Classcompo />
+            <Funcompo />
+            <Funcompo />
+        </div>
     )
 }
 
