f34629f78c809a21cd91d001c051a65a74a817f9
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-06-18 16:40:13 +0530

    project

--- React/cental/src/Admin/Apages/Blogmange.jsx
+++ React/cental/src/Admin/Apages/Blogmange.jsx
@@ -17,6 +17,31 @@ function Blogmange() {
         setblogs(res.data)
     }
 
+    const [blogdata, setblogdata] = useState({
+        id: "",
+        title: "",
+        desc: "",
+        comment: "",
+        name: "",
+        date: "",
+        img: ""
+    })
+
+    // get iduvasal product
+    const getdata = async (id) => {
+        const res = await axios.get(`http://localhost:3000/blog/${id}`)
+        console.log(res.data)
+        setblogdata(res.data)
+    }
+
+    const removepro=async(id)=>{
+        const res = await axios.delete(`http://localhost:3000/blog/${id}`)
+        console.log(res.data)
+        fetchdata()
+    }
+
+
+
 
     return (
         <div>
@@ -40,17 +65,17 @@ function Blogmange() {
                             blogs && blogs.map((data) => {
                                 console.log(data)
                                 return (
-                                    <tr  className='text-center'>
+                                    <tr className='text-center'>
                                         <th scope="row">{data.id}</th>
                                         <td>{data.name}</td>
                                         <td>{data.title}</td>
                                         <td>{data.date}</td>
-                                         <td>{data.comment}</td>
-                                         <td>
-                                            <button className='btn btn-info'>View</button>
+                                        <td>{data.comment}</td>
+                                        <td>
+                                            <button className='btn btn-info' data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => getdata(data.id)}>View</button>
                                             <button className='btn btn-success mx-2'>Edit</button>
-                                            <button className='btn btn-danger'>Delete</button>
-                                         </td>
+                                            <button className='btn btn-danger' onClick={()=>removepro(data.id)}>Delete</button>
+                                        </td>
                                     </tr>
                                 )
                             })
@@ -58,7 +83,38 @@ function Blogmange() {
 
                     </tbody>
                 </table>
-
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                 <div className="col-lg-12" data-wow-delay="0.1s">
+                                            <div className="blog-item">
+                                                <div className="blog-img">
+                                                    <img src={blogdata.img} className="img-fluid rounded-top w-100" style={{height:"200px"}} alt="Image" />
+                                                </div>
+                                                <div className="blog-content rounded-bottom p-4">
+                                                    <div className="blog-date">{blogdata.date}</div>
+                                                    <div className="blog-comment my-3">
+                                                        <div className="small"><span className="fa fa-user text-primary" /><span className="ms-2">{blogdata.name}</span></div>
+                                                        <div className="small"><span className="fa fa-comment-alt text-primary" /><span className="ms-2">{blogdata.comment} Comments</span></div>
+                                                    </div>
+                                                    <a href="#" className="h4 d-block mb-3">{blogdata.title}</a>
+                                                    <p className="mb-3">{blogdata.desc}</p>
+                                                    <a href="#" className>Read More  <i className="fa fa-arrow-right" /></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
