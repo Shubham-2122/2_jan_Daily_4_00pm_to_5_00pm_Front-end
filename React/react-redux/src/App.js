03cfd0442325817dd3e9246eac6d710967f1970c
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-07-02 16:42:30 +0530

    redux

--- React/react-redux/src/App.js
+++ React/react-redux/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Count from "./features/Counter/Count";
 import Count2 from "./features/Counter/Count2";
+import Todolist from "./features/Todo/Todolist";
+import Addtodo from "./features/Todo/Addtodo";
 
 function App() {
   return (
     <div className="App">
       <h1>hello this Redux app</h1>
-      <Count />
-      <Count2 />
+      {/* <Count />
+      <Count2 /> */}
+      <Addtodo />
+      <Todolist />
     </div>
   );
 }
--- React/react-redux/src/app/store.js
+++ React/react-redux/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  countSlice  from "../features/Counter/countSlice";
+import  todoSlice  from "../features/Todo/todoSlice";
 
 export const store = configureStore({
     reducer:{
-        counter : countSlice
+        counter : countSlice,
+        todo : todoSlice
     }
 })
