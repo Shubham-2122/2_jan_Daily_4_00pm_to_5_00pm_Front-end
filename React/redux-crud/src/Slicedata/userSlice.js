8df13b4d611858da416e7af1e5f4f773a91c2db8
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-07-03 17:08:44 +0530

    redux-crud

---
+++ React/redux-crud/src/Slicedata/userSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const userSlice = createSlice({
+    name:"userDetails",
+    initialState:{
+        user : [],
+        loading : false,
+        error : ""
+    },
+    reducers:{
+
+    }
+})
+
+export const {} = userSlice.actions;
+
+export default userSlice.reducer;
